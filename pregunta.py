eea8f500dbb7c65be25e3af2cbc022f1c9fd154b
anfrestrepora <anfrestrepora@unal.edu.co>
2024-03-15 22:10:31 -0500

    Intento 1

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,19 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
+    #Lectura DF, renombrar columnas
 
-    #
-    # Inserte su código aquí
-    #
-
+    df=pd.read_fwf('clusters_report.txt', skiprows=4,index_col=False, names=['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave'])
+    # Llenar valor inf y faltantes, y convertir en entero la columna clauster
+    df['cluster']=df['cluster'].ffill().astype(int)
+    # Eliminar duplicados la columna clauster
+    df=df.drop_duplicates(subset=['cluster'])
+    #Convertir cantidad de palabras en entero
+    df['cantidad_de_palabras_clave']=df['cantidad_de_palabras_clave'].astype(int)
+    #Eliminar procentajes, espacios y convertir la columna porcentaje en float
+    df['porcentaje_de_palabras_clave']=df['porcentaje_de_palabras_clave'].str.replace("%","").str.replace(",",".").astype(float)
+    #Separar palabras unicamente por espacio y eliminar puntos para que las palabras claves, esten separadas unicamente por ,
+    df['principales_palabras_clave']=df.groupby(['cluster'])['principales_palabras_clave'].transform(lambda x: " ".join(x)).str.replace(".","")
+    
     return df
